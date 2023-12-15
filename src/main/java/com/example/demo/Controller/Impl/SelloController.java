12f7d4116761b112fbbb201bb002ca36f5ec045f
facundodg <facundo_dg10@hotmail.com>
2023-12-15 12:37:13 -0300

    add auth

--- src/main/java/com/example/demo/Controller/Impl/SelloController.java
+++ src/main/java/com/example/demo/Controller/Impl/SelloController.java
@@ -6,7 +6,10 @@ import com.example.demo.Model.DTO.SelloDTO;
 import com.example.demo.Service.Interfaz.SelloServicios;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -15,10 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class SelloController implements SelloApi {
 
     private final SelloServicios selloServicios;
+    private static final Logger logger = LoggerFactory.getLogger(DemoRest.class);
 
     @Override
     public ResponseEntity<SelloDTO> generateSello(DatosDTO datos) throws Exception {
         log.info("[SelloController - GenerarSello]");
+        var auth =  SecurityContextHolder.getContext().getAuthentication();
+        logger.info("Datos del Usuario: {}", auth.getPrincipal());
+        logger.info("Datos de los Roles {}", auth.getAuthorities());
+        logger.info("Esta autenticado {}", auth.isAuthenticated());
         return ResponseEntity.ok(selloServicios.generarSello(datos));
     }
 
